8bed173c231dc50dac43c2f4f441620a229065a8
JoryAlharbi <144896456+JoryAlharbi@users.noreply.github.com>
2023-09-14 18:26:48 +0300

    Add files via upload

---
+++ Dataset Description   (1).r
@@ -0,0 +1,24 @@
+***
+ "Dataset Description"
+***
+
+## The goal of collecting this dataset 
+
+we will use this data set to assist financial institutions make informed decisions regarding loan
+approvals by analyzing customers’ payment behavior, as well as all their relevant attributes such 
+as age, income, loan amount, employment status, etc.By utilizing this data, predictive models can
+be built to forecast customers’ creditworthiness and may be used as a tool for risk assessment and
+decision-making.
+
+## The source of the dataset, include link\URL 
+
+The dataset was sourced from the kaggle website in this URL:
+https://www.kaggle.com/datasets/ppb00x/credit-risk-customers 
+
+## General information 
+
+1- Number of Attributes: 21
+2- Number of Objects: 1001
+3- Type of Attributes:
+
+4- class name and lable: The "class" which describes whether the customer is a good or bad credit risks. 
