fe90924f8473ac66ca6c060d80093377e2196e2d
JoryAlharbi <144896456+JoryAlharbi@users.noreply.github.com>
2023-10-06 10:10:23 +0300

    Update Rnotebook.R

--- Rnotebook.R
+++ Rnotebook.R
@@ -18,8 +18,6 @@ decision-making.
 
 The dataset was sourced from the kaggle website in this URL:
 https://www.kaggle.com/datasets/ppb00x/credit-risk-customers 
-### Project Motivation:
-we will use this data set to assist financial institutions make informed decisions regarding loan approvals and deciding weather a customer has good or bad cedit risks by analyzing customers’ payment behavior, as well as all their relevant attributes such as age, income, loan amount, employment status, etc. By utilizing this data, predictive models can be built to forecast customers’ creditworthiness and may be used as a tool for risk assessment and decision-making.
 
 ## General information 
 
