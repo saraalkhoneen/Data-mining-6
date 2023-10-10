2ecc1e6112f0ac977f2b4287a9378084a8bef916
Omaimah <144998792+omaimah1234@users.noreply.github.com>
2023-10-10 13:37:53 +0300

    Update Rnotebook.R

--- Rnotebook.R
+++ Rnotebook.R
@@ -157,9 +157,7 @@ dataset$duration <- min_max_scaling(dataset$duration)
 ```{r}
 hist(dataset$age, main = "Histogram of Age", xlab = "Age", ylab = "Frequency", col = "lightblue")
 ```
-We ceated a histogram for the "Age" attribute for its importance in deciding a customers credits risks 
-, what we learned from the histogram is the age distribution of our dataset, which is an important factor in 
-deciding the credit risks of a customer. 
+We created a histogram for the "Age" variable to show the age groups for our sample of customers to give a better understanding for our overall variables values and results.
 
 ```{r}
 barplot(table(dataset$checking_status), main = "Bar Plot of Checking Status", 
